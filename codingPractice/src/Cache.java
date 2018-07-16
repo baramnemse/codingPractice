1a7e335b503640a64e54e182cd05372a5cf23471
wonjin <wonjin@smarf.kr>
2018-07-16 22:49:21 +0900

    LRU

--- codingPractice/src/Cache.java
+++ codingPractice/src/Cache.java
@@ -1,9 +1,58 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Cache {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub\
+		System.out.println(getRunningTime(3, new String[] { "Jeju", "Pangyo",
+				"Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork",
+				"LA" }));
+		System.out.println(getRunningTime(0, new String[] { "Jeju", "Pangyo",
+				"Seoul", "NewYork", "LA" }));
+		System.out.println(getRunningTime(5, new String[] { "Jeju", "Pangyo",
+				"Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome",
+				"Paris", "Jeju", "NewYork", "Rome" }));
 
+	}
+
+	/**
+	 * LRU
+	 * @param i
+	 * @param input
+	 * @return
+	 */
+	private static int getRunningTime(int i, String input[]) {
+		int time = 0;
+		int runningTime = 0;
+		Map cache = new HashMap();
+		for (String city : input) {
+			Object cacheResult = cache.get(city);
+			if (cacheResult != null) {
+				// 캐시 시간만 업데이트
+				runningTime++;
+			} else {
+				// 가장 오래된 캐시 제거
+				if (cache.size() >= i) {
+					int oldTime = Integer.MAX_VALUE;
+					String doomKey = null;
+					Set set = cache.keySet();
+					for (Object key : set) {
+						if ((int) cache.get(key) < oldTime) {
+							oldTime = (int) cache.get(key);
+							doomKey = (String) key;
+						}
+					}
+					cache.remove(doomKey);
+				}
+				runningTime += 5;
+			}
+			cache.put(city, time);
+			time++;
+		}
+		return runningTime;
 	}
 	// LRU(least recently used) 오래된것 중 안쓰는것 삭제
 	// FIFO(first in first out) 오래된 데이터 삭제
--- codingPractice/src/QRemind.java
+++ codingPractice/src/QRemind.java
@@ -5,37 +5,35 @@ public class QRemind {
 	static Map resultMatrix = new HashMap();
 
 	public static void main(String[] args) {
-		int result = getMaxValue(new int[][] { { 5, 10 }, { 4, 40 }, { 6, 30 }, { 3, 50 } }, 4, 10);
+		int result = getMaxValue(new int[][] { { 5, 10 }, { 4, 40 }, { 6, 30 },
+				{ 3, 50 } }, 4, 10);
 		System.out.println(result);
 
 	}
 
 	static int getMaxValue(int[][] input, int i, int maxWeight) {
-		Object resultObject = resultMatrix.get(new int[] { i, maxWeight });
-		// 캐시에 있는지 확인하여 있으면 캐시값을 리턴한다.
-		if (resultObject != null) {
-			return (int) resultObject;
+		Map resultMap = new HashMap();
+		// 캐시에 있는지 확인
+		Object result = resultMap.get(new int[i][maxWeight]);
+		if (result != null) {
+			return (int) result;
 		} else {
-			// 캐시에 없을경우 점화식을 이용하여 해결한다.
 			int weight = input[i - 1][0];
 			int value = input[i - 1][1];
-			// i가 1인 경우 점화식을 이용할 수 없으므로 직접 계산한다
+			// i가 1인 경우 계산필요
 			if (i == 1) {
-				resultObject = (maxWeight / weight) * value;
-				resultMatrix.put(new int[] { i, maxWeight }, (int) resultObject);
-				return (int) resultObject;
+				return (maxWeight / weight) * value;
 			}
+			// 캐시에 없는 경우 재귀식으로 해결
 
-			// i 번째 무게를 더해도 최대값을 넘기지 않으므로 점화식으로 계산
 			if (maxWeight >= weight) {
-				return Math.max(getMaxValue(input, i - 1, maxWeight - weight) + value,
-						getMaxValue(input, i - 1, maxWeight));
+				// 새로 계산한값과 이전값중에 큰값이 이윤을 최대화한 값이다
+				return Math.max(getMaxValue(input, i - 1, maxWeight - weight)
+						+ value, getMaxValue(input, i - 1, maxWeight));
 			} else {
-				// 제한값을 넘치므로 i-1 결과값과 같다
-				return (int) getMaxValue(input, i - 1, maxWeight);
+				// 무게가 초과하므로 이전 값이 최고다
+				return getMaxValue(input, i - 1, maxWeight);
 			}
 		}
-
 	}
-
 }
