1d39d2ef305e6a310e4ace40a720581eb491294c
wonjin <wonjin@smarf.kr>
2018-07-21 19:00:49 +0900

    .

--- codingPractice/src/DPCoinChance.java
+++ codingPractice/src/DPCoinChance.java
@@ -16,7 +16,7 @@ public class DPCoinChance {
 	static int getMinCoinChangeWay(int[] coin, int n, int k) {
 		int cache[] = new int[k + 1];
 		// 가장 작은 동전으로 바꿨을 경우 동전의 갯수 보다 1크게
-		Arrays.fill(cache, k+1);
+		Arrays.fill(cache, k + 1);
 		cache[0] = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = coin[i]; j <= k; j++) {
@@ -30,7 +30,9 @@ public class DPCoinChance {
 	public static void main(String[] args) {
 		System.out.println(getMaxCoinChangeWay(new int[] { 2, 3, 5 }, 3, 10));
 		System.out.println(getMinCoinChangeWay(new int[] { 2, 3, 5 }, 3, 10));
+		System.out.println(getMaxCoinChangeWay(new int[] { 41, 34, 46, 9, 37,
+				32, 42, 21, 7, 13, 1, 24, 3, 43, 2, 23, 8, 45, 19, 30, 29, 18,
+				35, 11 }, 24, 250));
 
 	}
-
 }
--- codingPractice/src/DPLongestIncreasingSubsequence.java
+++ codingPractice/src/DPLongestIncreasingSubsequence.java
@@ -7,25 +7,27 @@
 public class DPLongestIncreasingSubsequence {
 
 	public static void main(String[] args) {
-		int[] input = new int[] { 10, 20, 10, 30, 20, 50 };
-		// int[] input = new int[] { 5, 4, 3, 2, 1, 6, 7, 8 };
+//		int[] input = new int[] { 2, 7, 4, 3, 8 };
+		 int[] input = new int[] { 5, 4, 3, 2, 1, 6, 7, 8 };
 
+		System.out.println(getLIS(input));
+	}
+
+	static int getLIS(int[] input) {
 		int[] cache = new int[input.length + 1];
 		int max = 0;
-		for (int i = 1; i < input.length; i++) {
-			int min = 0;
+		cache[0] = 1;
+		for (int i = 0; i < input.length; i++) {
+//			cache[i] = 1;
 			for (int j = 0; j < i; j++) {
 				if (input[i] > input[j]) {
-					if (min < cache[j]) {
-						min = cache[j];
-					}
+					cache[i] = Math.max(cache[i], cache[j] + 1);
 				}
 			}
-			cache[i] = min + 1;
 			max = Math.max(max, cache[i]);
 
 		}
-		System.out.println(max);
+		return max;
 	}
 
 }
