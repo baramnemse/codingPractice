577b87f4928d6b97751aa1e2b6f7cdd160a823d1
wonjin <wonjin@smarf.kr>
2018-07-27 18:01:00 +0900

    .

--- codingPractice/src/QRemind.java
+++ codingPractice/src/QRemind.java
@@ -7,8 +7,8 @@ import java.util.Map;
 public class QRemind {
 	public static void main(String[] args) {
 		int input[] = { 66, 10, 1, 34, 5, -10, 44, 23 };
-		// quickSort(input, 0, input.length - 1);
-		mergeSort(input, 0, input.length - 1);
+		int size = input.length;
+		sort(input, 0, size - 1);
 		System.out.println(Arrays.toString(input));
 
 	}
@@ -19,56 +19,23 @@ public class QRemind {
 		input[end] = temp;
 	}
 
-	static void quickSort(int input[], int start, int end) {
+	static void sort(int input[], int start, int end) {
+		final int pivot = (start + end) / 2;
 		if (end > start) {
-
-			int pivot = (start + end) / 2;
-			int left = start;
-			int right = end;
-
-			// 교차유도를 위해 같은것도 포함
-			if (left <= right) {
-				// 왼쪽에서 큰것 찾기
-				while (input[left] < input[pivot]) {
-					left++;
-				}
-				// 오른쪽에서 작은것 찾기
-				while (input[right] > input[pivot]) {
-					right--;
-				}
-				if (left <= right) {
-					swap(input, left, right);
-					left++;
-					right--;
-				}
-			}
-			// 교차된상태로 루프를 빠져나오기때문에 start right left end 순이 된다.
-			quickSort(input, start, right);
-			quickSort(input, left, end);
-		}
-	}
-
-	static void mergeSort(int input[], int start, int end) {
-		if (end > start) {
-			int pivot = (start + end) / 2;
-			// 왼쪽과 오른쪽을나눔
-			mergeSort(input, start, pivot);
-			mergeSort(input, pivot + 1, end);
-			// 합치면서 소트
+			sort(input, start, pivot);
+			sort(input, pivot + 1, end);
 			merge(input, start, end, pivot + 1);
 		}
 	}
 
 	static void merge(int input[], int start, int end, int pivot) {
-		// 결과저장소
-		int result[] = new int[end - start + 1];
 		int size = end - start + 1;
-		// 저정소에 저장한양
-		int count = 0;
+		int result[] = new int[size];
 		int left = start;
 		int right = pivot;
+		int count = 0;
+
 		while (count < size) {
-			// 왼쪽에 작은것이 있다
 			if (input[left] < input[right]) {
 				result[count] = input[left];
 				left++;
@@ -77,18 +44,17 @@ public class QRemind {
 				right++;
 			}
 			count++;
-			// 왼쪽만남은 경우 확인
-			if (right > end && count < size) {
+			// 왼쪽만 남았는지 확인
+			if (right >= end && count < size) {
 				System.arraycopy(input, left, result, count, size - count);
 				count += (size - count);
 			}
-			// 오른쪽만남은 경우 확인
-			if (left > pivot && count < size) {
+
+			if (left >= pivot && count < size) {
 				System.arraycopy(input, right, result, count, size - count);
 				count += (size - count);
 			}
 		}
-		// 최종결과 복사
 		System.arraycopy(result, 0, input, start, size);
 	}
 }
