b841a3f1d05593314eb8ce469d0ff5281f295429
wonjin <wonjin@smarf.kr>
2018-09-27 07:34:56 +0900

    Amazon 225

---
+++ codingPractice/src/Q225.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+/**
+ * 문자열압축
+ * 
+ * @author Wonjin Heo
+ *
+ */
+public class Q225 {
+	public static void main(String[] args) {
+		System.out.println(true == "abc".equals(zipString("abc")));
+		System.out.println(true == "a1b6c1".equals(zipString("abbbbbbc")));
+	}
+
+	static String zipString(String text) {
+		StringBuffer result = new StringBuffer();
+		char textBuffer[] = text.toCharArray();
+		char prevChar = 0;
+		int count = 1;
+		for (char c : textBuffer) {
+			if (prevChar == c) {
+				count++;
+			} else {
+				if (prevChar != 0) {
+					result.append(prevChar);
+					result.append(count);
+					count = 1;
+				}
+			}
+			prevChar = c;
+		}
+		result.append(prevChar);
+		result.append(count);
+
+		if (text.length() < result.toString().length()) {
+			return text;
+		} else {
+			return result.toString();
+		}
+	}
+
+	static boolean checkSerial(char[] text1, char[] text2) {
+		Arrays.sort(text1);
+		Arrays.sort(text2);
+		return new String(text1).equals(new String(text2));
+	}
+
+}
